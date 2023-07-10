746df0a1f6ac756e2bca7772cbb3cfdd08b3cc49
SangeethaShanmugm <MY_NAME@example.com>
2023-07-10 12:37:52 +0530

    updated fs

--- Inbuilt_Package/fs.js
+++ Inbuilt_Package/fs.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 
 const quote = "No beauty shines brighter than that of a good heart 🥳!!!";
 
-fs.writeFile("./awesome.ppt", quote, (err) => {
-  console.log("Completed Writing awesome.html");
-});
+// fs.writeFile("./awesome.ppt", quote, (err) => {
+//   console.log("Completed Writing awesome.html");
+// });
 
 const quote2 = "Live more, worry less 🥳🥳🥳";
 //Task- 1
@@ -16,12 +16,46 @@ const quote2 = "Live more, worry less 🥳🥳🥳";
 // ....
 // text-10.html
 
-for (let i = 1; i <= 10; i++) {
-  fs.writeFile(`./backup/text-${i}.html`, quote2, (err) => {
-    console.log(`Completed Writing text-${i}.html`);
-  });
-}
+// for (let i = 1; i <= 10; i++) {
+//   fs.writeFile(`./backup/text-${i}.html`, quote2, (err) => {
+//     console.log(`Completed Writing text-${i}.html`);
+//   });
+// }
 
+// break - 10 mins -> 11:55, task - 15 mins - 12:10
 
 //Task-2
 // node fs.js 20 -> 20 files to be created | note-1.txt, note-2.txt...note-20.txt
+
+// const [, , noOfFiles] = process.argv;
+// console.log(noOfFiles);
+// for (let i = 1; i <= noOfFiles; i++) {
+//   fs.writeFile(`./backup/note-${i}.txt`, quote2, (err) => {
+//     console.log(`Completed Writing note-${i}.txt`);
+//   });
+// }
+
+//read a file
+
+// fs.readFile("./cool123.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     console.log("Error❌", err);
+//   }
+//   console.log("The content of the file is =>", data);
+// });
+
+//append the file
+
+// const niceQuote = "\nMake everyday a little less ordinarily";
+
+// fs.appendFile("./nice.txt", niceQuote, (err) => {
+//   console.log("Completed writing nice.txt");
+// });
+
+fs.unlink("./toRemove.txt", (err) => {
+  console.log("Deleted successfully");
+});
+
+fs.readdir("./backup", (err, files) => {
+  console.log("All file names", files);
+});

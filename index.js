f6e4d85dd9cea72fd88eabd3c72a56fd68123b93
SangeethaShanmugm <MY_NAME@example.com>
2023-07-12 12:34:09 +0530

    added post, delete , mongo connect

--- index.js
+++ index.js
@@ -1,97 +1,28 @@
-const express = require("express"); // 3rd party package
+// const express = require("express"); // 3rd party package
+// const { MongoClient } = require("mongodb");
+import express from "express";
+import { MongoClient } from "mongodb";
 const app = express();
 const PORT = 9000;
 // req => what we send to Server
 // res => what we receive from Server
 
-const movies = [
-  {
-    id: "100",
-    name: "RRR",
-    poster:
-      "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG",
-    rating: 8.8,
-    summary:
-      "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.",
-    trailer: "https://www.youtube.com/embed/f_vbAtFSEc0",
-    language: "English",
-  },
-  {
-    id: "101",
-    name: "Iron man 2",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
-    rating: 7,
-    summary:
-      "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
-    trailer: "https://www.youtube.com/embed/wKtcmiifycU",
-    language: "English",
-  },
-  {
-    id: "102",
-    name: "No Country for Old Men",
-    poster:
-      "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
-    rating: 8.1,
-    summary:
-      "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
-    trailer: "https://www.youtube.com/embed/38A__WT3-o0",
-    language: "Hindi",
-  },
-  {
-    id: "103",
-    name: "Jai Bhim",
-    poster:
-      "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
-    summary:
-      "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
-    rating: 8.8,
-    trailer: "https://www.youtube.com/embed/nnXpbTFrqXA",
-    language: "English",
-  },
-  {
-    id: "104",
-    name: "The Avengers",
-    rating: 8,
-    summary:
-      "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
-    poster:
-      "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
-    trailer: "https://www.youtube.com/embed/eOrNdBpGMv8",
-    language: "Tamil",
-  },
-  {
-    id: "105",
-    name: "Interstellar",
-    poster: "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
-    rating: 8.6,
-    summary:
-      "When Earth becomes uninhabitable in the future, a farmer and ex-NASA\n pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team\n of researchers, to find a new planet for humans.",
-    trailer: "https://www.youtube.com/embed/zSWdZVtXT7E",
-    language: "Hindi",
-  },
-  {
-    id: "106",
-    name: "Baahubali",
-    poster: "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
-    rating: 8,
-    summary:
-      "In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy.",
-    trailer: "https://www.youtube.com/embed/sOEg_YZQsTI",
-    language: "Tamil",
-  },
-  {
-    id: "107",
-    name: "Ratatouille",
-    poster:
-      "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
-    rating: 8,
-    summary:
-      "Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
-    trailer: "https://www.youtube.com/embed/NgsQ8mVkN8w",
-    language: "Telugu",
-  },
-];
+//Mongodb Connection
+const MONGO_URL = "mongodb://0.0.0.0:27017";
+//mongodb://localhost
+//mongodb://localhost:27017
+//mongodb://0.0.0.0:27017
+async function createConnection() {
+  const client = new MongoClient(MONGO_URL);
+  await client.connect();
+  console.log("Mongodb is Connected");
+  return client;
+}
+
+const client = await createConnection();
+
+//interceptor | converting body to json | Inbuilt middleware
+app.use(express.json());
 
 //REST Api endpoints
 
@@ -106,25 +37,61 @@ app.get("/", (req, res) => {
 // /movies?language=English&rating=8 => filter by language & rating ✅
 
 //get all movies
-app.get("/movies", (req, res) => {
+app.get("/movies", async (req, res) => {
   const { language, rating } = req.query;
   console.log(req.query, language);
-  let filteredMovies = movies;
-  if (language) {
-    filteredMovies = movies.filter((mv) => mv.language == language);
+  // let filteredMovies = movies;
+  // if (language) {
+  //   filteredMovies = movies.filter((mv) => mv.language == language);
+  // }
+  // if (rating) {
+  //   filteredMovies = movies.filter((mv) => mv.rating == rating);
+  // }
+  if (req.query.rating) {
+    req.query.rating = +req.query.rating;
   }
-  if (rating) {
-    filteredMovies = movies.filter((mv) => mv.rating == rating);
-  }
-  res.send(filteredMovies);
+  const movie = await client
+    .db("b47-wd")
+    .collection("movies")
+    .find(req.query)
+    .toArray();
+  res.send(movie);
 });
 
 //get movie by id
-app.get("/movies/:id", (req, res) => {
+app.get("/movies/:id", async (req, res) => {
   const { id } = req.params;
   console.log(req.params, "ID=", id);
-  const movie = movies.find((mv) => mv.id == id);
+  //db.movies.findOne({id:"100"})
+  // const movie = movies.find((mv) => mv.id == id);
+  const movie = await client
+    .db("b47-wd")
+    .collection("movies")
+    .findOne({ id: id });
+  movie
+    ? res.send(movie)
+    : res.status(404).send({ message: "No movies found" });
+});
+
+//delete book by id
+app.delete("/movies/:id", async (req, res) => {
+  const { id } = req.params;
+  const movie = await client
+    .db("b47-wd")
+    .collection("movies")
+    .deleteOne({ id: id });
   res.send(movie);
+});
+
+//add movies
+app.post("/movies", async (req, res) => {
+  const newMovie = req.body;
+  console.log(newMovie);
+  const result = await client
+    .db("b47-wd")
+    .collection("movies")
+    .insertMany(newMovie);
+  res.send(result);
 });
 
 app.listen(PORT, () => console.log("Server started on PORT", PORT));
